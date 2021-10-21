9f8a107ce4ac965b12d537953b6b386d33f0ed35
CSI-Diego-Cedeno <89210542+CSI-Diego-Cedeno@users.noreply.github.com>
2021-10-21 13:02:57 -0400

    Projectile

---
+++ Projects/Projectile-Motion/ExperimentData.py
@@ -0,0 +1,9 @@
+class ExperimentData:
+    def __init__(self, Gunname:str, Guncartridge:str, Round:str, RoundVelocity_ms:int, Building:str, BuildingHeight_m:int, Gravity_ms:int):
+      self.Gunname = Gunname
+      self.Guncartridge = Guncartridge
+      self.Round = Round
+      self.RoundVelocity_ms = RoundVelocity_ms
+      self.Building = Building
+      self.BuildingHeight_m = BuildingHeight_m
+      self.Gravity_ms = Gravity_ms
--- Projects/Projectile-Motion/Projectile-Motion.py
+++ Projects/Projectile-Motion/Projectile-Motion.py
@@ -1,4 +1,10 @@
 import math
+from ExperimentData import ExperimentData
+
+import os
+from pathlib import Path
+
+import json
 
 # Gunname = "Knight's Armament Company SR-25"
 # Guncartridge = "7.62x51mm NATO"
@@ -8,24 +14,40 @@ import math
 # BuildingHeight_m = 472
 # Gravity_ms= 9.81
 
-def gunBuilding (Gunname:str, Guncartridge:str, Round:str, RoundVelocity_ms:str, Building:str
-, BuildingHeight_m:str, Gravity_ms:str):
-
-    Time = math.sqrt((2 * BuildingHeight_m) / Gravity_ms)
-    Distance = (RoundVelocity_ms * Time)
-
-
+# Here in calculating the time by plugging in the formula with the variables defined on top. But first, you need to import the fuction of the square root. 
+def ProjectileFunction(experimentData:ExperimentData):
+    time_s = math.sqrt((2 * experimentData.BuildingHeight_m) / experimentData.Gravity_ms)
+    Distance_m = (experimentData.RoundVelocity_ms * time_s)
     print(f"""Chris is planning on killing an old lady on Central park.
-    He will use an {Gunname} which uses a cartridge of {Guncartridge}. 
-    The round of this weapon is {Round}and the velocity of the round is {RoundVelocity_ms}m/s.
-    He will shoot the gun from {Building}, this building has a height of {BuildingHeight_m}m.
-    The gravity on earth is {Gravity_ms}m/s, the time it will take the gun to kill the lady is {Time},
-    and the distance it will run is {Distance}. Chris just killed the old lady.
+    He will use an {experimentData.Gunname} which uses a cartridge of {experimentData.Guncartridge}. 
+    The round of this weapon is {experimentData.Round}and the velocity of the round is {experimentData.RoundVelocity_ms}m/s.
+    He will shoot the gun from {experimentData.Building}, this building has a height of {experimentData.BuildingHeight_m}m.
+    The gravity on earth is {experimentData.Gravity_ms}m/s, the time it will take the gun to kill the lady is {time_s},
+    and the distance it will run is {Distance_m}. Chris just killed the old lady.
     """)
 
+# experimentalData = {
+#  "Gunname" : "Knight's Armament Company SR-25",
+#  "Guncartridge" : "7.62x51mm NATO",
+#  "Round" : "7.62x51mm M993",
+#  "RoundVelocity_ms" : 910,
+#  "Building" : "Central Park Tower",
+#  "BuildingHeight_m" : 472,
+#  "Gravity_ms": 9.81
+# }
 
-gunBuilding ("Knight's Armament Company SR-25", "7.62x51mm NATO", "7.62x51mm M993", 910
-, "Central Park Tower", 472, 9.81)
+myDataSet = [
+     ExperimentData("Knight's Armament Company SR-25",)
+
+
+
+]
+
+MyOutputPath = Path(__file__).parents[0]
+MyOutputFilePath = os.path.join(myOutputPath , 'ExperimentData.json')
+
+with open(myOutputFilePath, 'w') as outfile:
+    json.dump(experimentData.__dict__ , outfile)
 
 
 
