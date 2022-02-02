1c2793059642d4a9a68d03b5d06f1f9d4a80b42b
CSI-Diego-Cedeno <89210542+CSI-Diego-Cedeno@users.noreply.github.com>
2022-02-02 12:01:44 -0400

    Create RequestDeserialization.py

---
+++ Projects/Web-Servers/RequestDeserialization.py
@@ -0,0 +1,30 @@
+import json, ssl
+import os
+from pathlib import Path
+import urllib.request
+from Stripe import Stripe
+
+# This is discouraged but it will avoid certificate validation (prevents error)
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# This is the URL from which we will request the data
+stripeURL = "https://random-data-api.com/api/stripe/stripe?size=100"
+
+# Create a list to populate with our data
+MyOutputPath = Path(__file__).parents[0]
+MyOutputFilePath = os.path.join(MyOutputPath , 'responses')
+os.mkdir(MyOutputFilePath)
+
+
+# Execute HTTP Request
+req = urllib.request.Request(stripeURL)
+requestData = json.loads(urllib.request.urlopen(req).read())
+
+# Loop over JSON items and Deserialize them into python objects
+for r in requestData:  
+    # Deserialize 
+    stripe:Stripe = Stripe(**r)
+    # Add object to list
+    stripe.append(stripe) 
+    # Print id
+    print(stripe.id)
