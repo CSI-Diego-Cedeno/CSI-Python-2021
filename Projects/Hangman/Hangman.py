a50d6907526038a04f2798f01e5b7394a90f2b18
CSI-Diego-Cedeno <89210542+CSI-Diego-Cedeno@users.noreply.github.com>
2022-02-24 00:20:24 -0400

    Update Hangman.py

--- Projects/Hangman/Hangman.py
+++ Projects/Hangman/Hangman.py
@@ -4,19 +4,23 @@ from pathlib import Path
 import urllib.request
 from Dessert import Dessert
 
+# This is used to prevent errors
 ssl._create_default_https_context = ssl._create_unverified_context
 
+# This URL is used to request a word from the class Dessert
 DessertURL = "https://random-data-api.com/api/dessert/random_dessert"
 
-
+# Here creates a variable to ask for data
 req = urllib.request.Request(DessertURL)
 requestData = json.loads(urllib.request.urlopen(req).read())
 
-# Deserialize  
-newDesert:Dessert = Dessert(**requestData)
+# Deserialize into a class
+newDessert:Dessert = Dessert(**requestData)
 
-print(newDessert.variety)
+# This is the variable created for all the incorrect characters
+attemptedCharacter = []
 
+# On this part are the each steps that creates the hangman
 Steps = ["""
 |---------|
 |         |
@@ -89,48 +93,58 @@ Steps = ["""
 """
 ]
 
-
+# This print starts the game
 print(Steps[0])
 
-print(newDessert.variety)
-
+# In this part it prints the word that it got from the data and puts each character an _
 print(len(newDessert.variety)*" _ ")
 
+# The function its used so that the game tells you the characters that are errors in the game
 def getInput():
     while(True):
+
       # ask for input
-      Character = input(f"name a letter for this dessert: ")
-      special_char = "[@_!#$%^&*()<>?/\|}{~:]"
+     Character = input(f"name a letter for this dessert: ")
+     special_char = "[@_!#$%^&*()<>?/\|}{~:]"
 
       # Validate input
-      if(len(Character) != 1):
-          print("error, just put a letter: ")
+     if(len(Character) != 1):
+          print("error, just type a letter: ")
           continue
 
-      if(Character.isnumeric()):
-          print("error, no numbers, just a letter: ")
-          continue
+     if(Character.isnumeric()):
+         print("error, no numbers, just type a letter: ")
+         continue
 
-      if(Character in special_char):
-          print("Error, no special charcter, just a letter: ")
-          continue
+     if(Character in special_char):
+        print("Error, no special charcter, just type a letter: ")
+        continue
+    
+    # This is for used character that were typed
+     attemptedCharacter.append(Character)
+     return Character
 
-      return Character
-
-
-print(getInput())
-
-
+# This function is used so that it stores every correct Character that you type
 def print_word():
     Tempt:str = " "
+    for Character in newDessert.variety:
+        if Character in attemptedCharacter:
+            Temp +="_"
+        else:
+            Temp += Character
+        return Temp
 
-    for Character in print_word:
+# This is used to store the erros you do when putting the Character, the function adds 1 to each one you get wrong
+def Print_stepsErrors():
+    error = 0
+    for Character in attemptedCharacter:
+        if(Character not in attemptedCharacter):
+            error += 1
 
-        for (matches) or in():
-            {Tempt=="_" or Character
-            
-            
-        return or print
-        Tempt}
+# The following part is the while True which will keep the game going and the game restarts
+while True:
+    getInput()
+    print_word()
+    Print_stepsErrors()
+    
 
-print(print_word())
