3b5120a4c045c1fd6692ad764607df006c2c91fb
CSI-Diego-Cedeno <89210542+CSI-Diego-Cedeno@users.noreply.github.com>
2022-03-17 11:57:57 -0400

    Update Snake.py

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -1,6 +1,12 @@
 import pygame
-pygame.init()
-dis=pygame.display.set_mode((400,300))
-pygame.display.update()
+pygame.init() #This initializes all the imported pygame modules
+dis=pygame.display.set_mode((400,300)) # Sets the dimensions for the game 
+pygame.display.update() #Here it updates the screen
+pygame.display.set_caption('Snake game by Edureka') #Will see this message on the display screen
+game_over=False 
+while not game_over:
+    for event in pygame.event.get():
+        print(event)   #prints out all the actions that take place on the screen
+ 
 pygame.quit()
 quit()
