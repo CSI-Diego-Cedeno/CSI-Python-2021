820eb98bf9f103840aa48727da9dfc2cdb0c5ca4
CSI-Diego-Cedeno <89210542+CSI-Diego-Cedeno@users.noreply.github.com>
2022-03-22 22:26:18 -0400

    Update Snake.py

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 #This initializes all the imported pygame modules
 pygame.init() 
@@ -8,15 +9,22 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 red=(255, 0, 0)
 
+#Dimensions of the game
+dis_width = 800
+dis_height = 600
+
 # Sets the dimensions for the game 
-dis=pygame.display.set_mode((800,600)) 
+dis=pygame.display.set_mode((dis_width, dis_width)) 
 #Will see this message on the display screen
 pygame.display.set_caption('Snake game by Edureka') 
 
 game_over=False 
 
-x1 = 300
-y1 = 300
+#the defined variables are width and height divided by 2
+x1 = dis_width/2
+y1 = dis_height/2
+
+snake_block= 10
 
 #These are variables that have the values of x and y coordinates
 x1_change = 0
@@ -24,7 +32,18 @@ y1_change = 0
 
 #This is used to keep time of the game
 clock = pygame.time.Clock()
+#The speed that the snake will have
+snake_speed = 30
 
+#font style for message
+font_style = pygame.font.SysFont(None, 50)
+
+#This function creates the message that will appear at game over and it creates size, color, and position of the text
+def message (msg, color):
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [dis_width/2, dis_height/2])
+
+#creates and starts the loop
 while not game_over:
     #Returns all the actions 
     for event in pygame.event.get(): 
@@ -35,30 +54,39 @@ while not game_over:
         if event.type == pygame.KEYDOWN: 
             #Moves the snake to the left 
             if event.key == pygame.K_LEFT: 
-                x1_change = -10
+                x1_change = -snake_block
                 y1_change = 0
             #Moves the snake to the right
             elif  event.key == pygame.K_RIGHT: 
-                 x1_change = 10
+                 x1_change = snake_block
                  y1_change = 0
             #Moves the snake up
             elif  event.key == pygame.K_UP: 
-                 y1_change = -10
+                 y1_change = -snake_block
                  x1_change = 0
             #Moves the snake down
             elif  event.key == pygame.K_DOWN: 
-                 y1_change = 10
+                 y1_change = snake_block
                  x1_change = 0
+    #defines limits for x and y coordinates of the snake, less or equal to the screen
+    if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+        game_over = True
 
     x1 += x1_change
     y1 += y1_change
     dis.fill(white)
     #This function draws the snake as a rectangle with color and size
-    pygame.draw.rect(dis, black, [x1, y1, 10, 10]) 
+    pygame.draw.rect(dis, black, [x1, y1, snake_block, snake_block]) 
     #Here it updates the screen
     pygame.display.update() 
 
-    clock.tick(30)
+    #Here the clock keeps the snake speed 
+    clock.tick(snake_speed)
+
+#Here it gives you the message that you lost and it updates it on the display in red
+message("You lost", red)
+pygame.display.update()
+time.sleep(2)
 
 #Here it quits the game
 pygame.quit() 
