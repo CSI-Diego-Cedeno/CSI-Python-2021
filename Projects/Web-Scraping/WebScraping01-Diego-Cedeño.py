82e9478a2b2da9e74704cfed407332110809e572
CSI-Diego-Cedeno <89210542+CSI-Diego-Cedeno@users.noreply.github.com>
2022-04-21 11:36:56 -0400

    02

--- Projects/Web-Scraping/WebScraping01-Diego-Cedeño.py
+++ Projects/Web-Scraping/WebScraping01-Diego-Cedeño.py
@@ -1,7 +1,7 @@
 # import webbrowser
 # webbrowser .open("https://www.elnuevodia.com")
 
-import requests 
+#import requests 
 # res = requests.get("https://www.gutenberg.org/cache/epub/67771/pg67771.txt")
 # print(len(res.text))
 # print(res.text[:300])
